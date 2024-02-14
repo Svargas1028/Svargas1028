7fb652eff78f58ba0db52755f9ffd24400b102ed
Svargas1028 <160050780+Svargas1028@users.noreply.github.com>
2024-02-14 16:21:04 -0500

    Add files via upload
    first submission

---
+++ P1LAB1_VargasSandra.py
@@ -0,0 +1,13 @@
+# Sandra Vargas
+# 2/14/2024
+# P1LAB1 
+# This program takes a first name as input and outputs a welcome message.
+
+# Get user input
+first_name = input("Enter your first name: ")
+
+# Output hello message
+print("Hello,", first_name + "!")
+
+# Output welcome message
+print("Welcome to Spring 2024 Web, Pgm, & Db Foundation (CTI-110-0903).")
